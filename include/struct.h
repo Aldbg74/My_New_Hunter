883d342af94b483e3f8b9907a027401c7aafb5ee
Aldbg74 <alexis.drago-beltran@epitech.eu>
2024-06-11 19:37:57 +0200

    [ADD] .h for struc files

---
+++ include/struct.h
@@ -0,0 +1,11 @@
+/*
+** EPITECH PROJECT, 2024
+** My_New_Hunter
+** File description:
+** struct.h
+*/
+
+#ifndef MY_NEW_HUNTER_STRUCT_H
+    #define MY_NEW_HUNTER_STRUCT_H
+
+#endif //MY_NEW_HUNTER_STRUCT_H
